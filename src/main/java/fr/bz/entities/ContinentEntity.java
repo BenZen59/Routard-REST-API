7bc5555a8b2ce3c86bf8cd943100628f3bebfa6c
BenZen59 <benoit.zenone@gmail.com>
2024-03-01 15:41:21 +0100

    commit 01/03 with little change

--- src/main/java/fr/bz/entities/ContinentEntity.java
+++ src/main/java/fr/bz/entities/ContinentEntity.java
@@ -3,8 +3,11 @@ package fr.bz.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity(name = "CONTINENT")
 @Getter
@@ -15,4 +18,8 @@ public class ContinentEntity {
     private String codeContinent;
     @Column(name = "NOM_CONTINENT")
     private String nomContinent;
+    @OneToMany(mappedBy = "continent")
+    private List<PaysEntity> pays;
+
+
 }
